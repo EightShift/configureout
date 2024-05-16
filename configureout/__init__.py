298b0cf7172107331fa6c116fc24d98ca18fa791
EightShift <35277031+EightShift@users.noreply.github.com>
2024-05-16 19:11:50 +0500

    Add files via upload

--- configureout/__init__.py
+++ configureout/__init__.py
@@ -1,38 +1,50 @@
 import json
 
 
-class BaseConfig():
-    def __init__(self, config):
-        self.__config = config
-        
-        for key, value in self.__config.items():
-            setattr(self, key, BaseConfig(value) if type(value) == dict else value)
+class Config(object):
+	def __init__(self, config_file=None, /, config=None, encoding='utf-8'):
+		if config_file:
+			with open(config_file, 'r', encoding=encoding) as config_file:
+				config = json.loads(config_file.read())
 
-    def __getitem__(self, key):
-        return getattr(self, key)
+		if type(config) != dict:
+			raise TypeError(f'Config must be a dict, not {type(config)}')
 
-    def keys(self):
-        for key in self.__config.keys():
-            yield key
+		self.__keys = list(config.keys())
+		self.__index = 0
 
-    def values(self):
-        for key in self.__config.keys():
-            yield getattr(self, key)
-    
-    def items(self):
-        for key in self.__config.keys():
-            yield key, getattr(self, key)
+		for k, v in config.items():
+			if isinstance(v, (list, tuple)):
+				setattr(self, k, [Config(config=x) if isinstance(x, dict) else x for x in v])
+			else:
+				setattr(self, k, Config(config=v) if isinstance(v, dict) else v)
 
+	def __getitem__(self, key):
+		return getattr(self, key)
 
-class Config(BaseConfig):
-    def __init__(self, config_file, encoding='utf-8'):
-        self.__file = config_file
+	def __len__(self):
+		return len(self.__keys)
+		
+	def __iter__(self):
+		return self
+	
+	def __next__(self):
+		if self.__index >= len(self.__keys):
+			self.__index = 0
+			raise StopIteration
 
-        self._load_config(encoding)
+		key = self.__keys[self.__index]
+		self.__index += 1
+		return key, getattr(self, key)
 
-    def _load_config(self, encoding):
-        with open(self.__file, "r", encoding=encoding) as config_file:
-            config = json.loads(config_file.read())
+	def get(self, key, default=None):
+		return getattr(self, key, default)
 
-            super().__init__(config)
+	def keys(self):
+		return (k for k in self.__keys)
 
+	def values(self):
+		return (getattr(self, k) for k in self.__keys)
+	
+	def items(self):
+		return ((k, getattr(self, k)) for k in self.__keys)
